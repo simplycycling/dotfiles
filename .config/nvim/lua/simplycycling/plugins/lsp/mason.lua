efad28fccabd797625f425b9c218104c576f63b5
Roger Sherman <rsherman@linux.com>
2026-02-06 16:57:26 +1000

    Updated for mason-lspconfig 2.0 api

--- .config/nvim/lua/simplycycling/plugins/lsp/mason.lua
+++ .config/nvim/lua/simplycycling/plugins/lsp/mason.lua
@@ -5,15 +5,10 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
-
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
 		local mason_tool_installer = require("mason-tool-installer")
 
-		-- enable mason and configure icons
 		mason.setup({
 			ui = {
 				icons = {
@@ -25,7 +20,6 @@ return {
 		})
 
 		mason_lspconfig.setup({
-			-- list of servers for mason to install
 			ensure_installed = {
 				"ts_ls",
 				"html",
@@ -38,16 +32,15 @@ return {
 				"prismals",
 				"pyright",
 			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true,
+			automatic_enable = true, -- Replaces automatic_installation
 		})
 
 		mason_tool_installer.setup({
 			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
+				"prettier",
+				"stylua",
+				"isort",
+				"black",
 				"pylint",
 				"eslint_d",
 			},
