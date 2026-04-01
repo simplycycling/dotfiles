350d898bf2d5743d7868ad2e0f0bf7de2c6e74d4
Roger Sherman <rsherman@linux.com>
2026-04-01 17:49:22 +1000

    Trying out a couple of zazencodes workflows

--- .config/nvim/lua/simplycycling/core/workflows.lua
+++ .config/nvim/lua/simplycycling/core/workflows.lua
@@ -1,1 +1,4 @@
+vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//]])<cr>")
+
 vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
+vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
