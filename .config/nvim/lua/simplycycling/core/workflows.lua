ed130fc998b0a2b3c257b62b1f7cb7d9d8d3792b
Roger Sherman <rsherman@linux.com>
2026-04-01 21:40:56 +1000

    More nifty workflows

--- .config/nvim/lua/simplycycling/core/workflows.lua
+++ .config/nvim/lua/simplycycling/core/workflows.lua
@@ -3,5 +3,5 @@ vim.keymap.set("n", "<leader>on", function()
 	vim.cmd([[1,/^\S/s/^\n\{1,}//e]])
 end)
 
-vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
 vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
+vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
