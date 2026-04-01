a815b0dae3800ccc4195020bbb844edb0031183c
Roger Sherman <rsherman@linux.com>
2026-04-01 18:50:15 +1000

    Remembering better days.

--- .config/nvim/lua/simplycycling/core/workflows.lua
+++ .config/nvim/lua/simplycycling/core/workflows.lua
@@ -1,4 +1,7 @@
-vim.keymap.set("n", "<leader>on", ":ObsidianTemplate note<cr> :lua vim.cmd([[1,/^\\S/s/^\\n\\{1,}//e]])<cr>")
+vim.keymap.set("n", "<leader>on", function()
+	vim.cmd("ObsidianTemplate note")
+	vim.cmd([[1,/^\S/s/^\n\{1,}//e]])
+end)
 
 vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
 vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
