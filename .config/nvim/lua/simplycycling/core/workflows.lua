c091ecd20990dcb9b64a22e75bfbbc690f16d5b5
Roger Sherman <rsherman@linux.com>
2026-04-01 22:14:10 +1000

    Going full zazencodes. Mostly.

--- .config/nvim/lua/simplycycling/core/workflows.lua
+++ .config/nvim/lua/simplycycling/core/workflows.lua
@@ -1,7 +1,38 @@
+--------------
+-- obsidian --
+--------------
+--
+-- >>> oo # from shell, navigate to vault (optional)
+--
+-- # NEW NOTE
+-- >>> on "Note Name" # call my "obsidian new note" shell script (~/bin/on)
+-- >>>
+-- >>> ))) <leader>on # inside vim, format note as template
+-- >>> ))) # add tag, e.g. fact / blog / video / etc..
+-- >>> ))) # add hubs, e.g. [[python]], [[machine-learning]], etc...
+-- >>> ))) <leader>of # format title
+--
+-- # END OF DAY/WEEK REVIEW
+-- >>> or # review notes in inbox
+-- >>>
+-- >>> ))) <leader>ok # inside vim now, move to zettelkasten
+-- >>> ))) <leader>odd # or delete
+-- >>>
+-- >>> og # organize saved notes from zettelkasten into notes/[tag] folders
+--
+-- convert note to template and remove leading white space
 vim.keymap.set("n", "<leader>on", function()
 	vim.cmd("ObsidianTemplate note")
 	vim.cmd([[1,/^\S/s/^\n\{1,}//e]])
 end)
-
+-- strip date from note title and replace dashes with spaces
+-- must have cursor on title
 vim.keymap.set("n", "<leader>of", ":s/\\(# \\)[^_]*_/\\1/ | s/-/ /g<cr>")
+-- search for files in full vault
 vim.keymap.set("n", "<leader>os", ':Telescope find_files search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
+vim.keymap.set("n", "<leader>oz", ':Telescope live_grep search_dirs={"/Users/rsherman/Documents/notes"}<cr>')
+-- for review workflow
+-- move file in current buffer to zettelkasten folder
+vim.keymap.set("n", "<leader>ok", ":!mv '%:p' /Users/rsherman/Documents/notes/zettelkasten<cr>:bd<cr>")
+-- delete file in current buffer
+vim.keymap.set("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>")
